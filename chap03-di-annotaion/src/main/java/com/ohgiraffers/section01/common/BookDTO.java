a8ec9c16de613535896e94e5a7b82aaecc61d621
JH5256 <skdltmvhs2@naver.com>
2024-03-25 13:05:18 +0900

    second commit

---
+++ chap03-di-annotaion/src/main/java/com/ohgiraffers/section01/common/BookDTO.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section01.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BookDTO {
+
+    private int sequence;
+    private int isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private Date createDate;
+}
